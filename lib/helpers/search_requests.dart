2597e1305eebf65fd8d05634cffad7d87feacf01
William Ballard <jacob.ballard.1999@gmail.com>
2022-03-28 17:28:20 -0400

    JSON still not working

--- lib/helpers/search_requests.dart
+++ lib/helpers/search_requests.dart
@@ -10,12 +10,14 @@ class SearchRequests {
   String apk = r'YTU2MjRlMzQtZWQxZi00ZTljLTk4NGItMDM4ZTg3MDQyZDhk';
   String b = '&limit=25&query=';
 
+  String c = '&type=tracks';
+
   ///v2.2/albums/Alb.54719066/images
 
   Future<List<SongObject>> search(String query) async {
     List<SongObject> songs = [];
 
-    String request = a + apk + b + query;
+    String request = a + apk + c + b + query;
 
     debugPrint("request" + request);
 
@@ -25,10 +27,30 @@ class SearchRequests {
   }
 
   Future<List<SongObject>> _search(String request) async {
-    final response = await http.get(Uri.parse(request));
+    var client = http.Client();
+    final response = await client.get(Uri.parse(request));
     List<SongObject> songs = [];
     if (response.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(response.body);
+      final decodedMore = json["search"] as Map;
+      final b = decodedMore["data"] as Map;
+      Map<String, dynamic> a = json["search"];
+
+      Map<String, dynamic> c = a["data"];
+
+      Map<String, dynamic> albums = c[0];
+      b.forEach((key, value) {
+        debugPrint(key + value);
+      });
+      // final allTracks = b["albums"] as Map;
+      // allTracks.forEach((key, value) {
+      //   songs.add(SongObject.fromJson(value));
+      // });
+
+      // Iterable l = jsonDecode(json as String);t
+      // songs = List<SongObject>.(tracks.map((key, value) => SongObject.fromJson(value)));
+      // Map<String, dynamic> json = jsonDecode(response.body);
+      debugPrint(json["meta"]);
       var test = json.entries;
       Map<String, dynamic> searches = jsonDecode(json["search"]);
       Map<String, dynamic> tracks = jsonDecode(searches['tracks']);
